76196d354c4702688aa936dece12ec474087f668
Nguyen Duc Hieu <hangnguyen16052001@gmail.com>
2025-04-28 18:22:01 +0700

    Initial project structure from Spring Initializr

---
+++ tiem-banh-online/src/main/java/com/tiembanhngot/tiem_banh_online/TiemBanhOnlineApplication.java
@@ -0,0 +1,13 @@
+package com.tiembanhngot.tiem_banh_online;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TiemBanhOnlineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TiemBanhOnlineApplication.class, args);
+	}
+
+}
---
+++ tiem-banh-online/src/test/java/com/tiembanhngot/tiem_banh_online/TiemBanhOnlineApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tiembanhngot.tiem_banh_online;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TiemBanhOnlineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
