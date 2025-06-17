ab1f4499d0cc3573b1fdc7cb70c0523c44d626ed
Nguyen Duc Hieu <hangnguyen16052001@gmail.com>
2025-06-17 16:23:15 +0700

    sua slug

--- tiem-banh-online/src/main/java/com/tiembanhngot/tiem_banh_online/repository/ProductRepository.java
+++ tiem-banh-online/src/main/java/com/tiembanhngot/tiem_banh_online/repository/ProductRepository.java
@@ -1,6 +1,7 @@
 package com.tiembanhngot.tiem_banh_online.repository;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
@@ -12,7 +13,7 @@ import com.tiembanhngot.tiem_banh_online.entity.Product;
 
 @Repository
 public interface ProductRepository extends JpaRepository<Product, Long> {
-
+    Optional<Product> findByName(String slug);
     List<Product> findByCategory(Category category);
     List<Product> findByCategoryAndIsAvailableTrue(Category category);
     List<Product> findByIsAvailableTrueOrderByCreatedAtDesc();
